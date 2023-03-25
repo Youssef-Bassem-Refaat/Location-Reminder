36bce76cd2e42b791febff972e57c7b8518eb5ef
Youssef Bassem <youssef.bassem.refaat@gmail.com>
2023-03-25 07:43:57 +0200

    Test Files
    Test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -22,6 +22,71 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-    //    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var remindersRepository: RemindersLocalRepository
+
+    private lateinit var database: RemindersDatabase
+
+    val reminder1 = ReminderDTO("Tit1", "Desc1", "location1", 1.1, 1.1)
+    val reminder2 = ReminderDTO("Tit2", "Desc2", "location2", 2.2, 2.2)
+    val reminder3 = ReminderDTO("Tit3", "Desc3", "location3", 2.2, 3.3)
+
+    @Before
+    fun setup() {
+        // testing with an in-memory database because it won't survive stopping the process
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        remindersRepository = RemindersLocalRepository(database.reminderDao(), Dispatchers.Main)
+    }
+
+    @After
+    fun cleanUp() {
+        database.close()
+    }
+
+    @Test
+    fun deleteAndGetReminders_deleteReminders_listIsEmpty() = runBlocking {
+        remindersRepository.deleteAllReminders()
+        val result = remindersRepository.getReminders()
+
+        result as Result.Success
+        Assert.assertThat(result.data.isEmpty(), `is`(true))
+    }
+
+    @Test
+    fun saveAndGetReminder_giveWrongId_ReturnError() = runBlocking {
+        // GIVEN - save reminer
+        remindersRepository.saveReminder(reminder1)
+
+        // WHEN  - check the reminder Id by wrong one
+        val result = remindersRepository.getReminder("1001")
+        result as Result.Error
+
+        // THEN - can not find reminder
+        Assert.assertThat(result.message, `is`("Reminder not found!"))
+    }
+
+
+
+    @Test
+    fun saveReminder_giveWrongId_retriveTheRightReminder() = runBlocking {
+        // GIVEN - save reminder
+        remindersRepository.saveReminder(reminder1)
+
+        // WHEN  - get reminder
+        val result = remindersRepository.getReminder(reminder1.id)
+        result as Result.Success
+
+        // THEN - reminder is the same with the one i created
+        Assert.assertThat(result.data.id, `is`(reminder1.id))
+        Assert.assertThat(result.data.title, `is`(reminder1.title))
+        Assert.assertThat(result.data.description, `is`(reminder1.description))
+        Assert.assertThat(result.data.latitude, `is`(reminder1.latitude))
+        Assert.assertThat(result.data.longitude, `is`(reminder1.longitude))
+    }
 }
--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -41,8 +41,79 @@ import org.mockito.Mockito.verify
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest : KoinTest{
+    private val dataSource: ReminderDataSource by inject()
+    private val reminder1 = ReminderDTO("Reminder1", "Description1", "Location1", 1.0, 1.0,"1")
+    private val reminder2 = ReminderDTO("Reminder2", "Description2", "location2", 2.0, 2.0, "2")
+    private val reminder3 = ReminderDTO("Reminder3", "Description3", "location3", 3.0, 3.0, "3")
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
-}
+    @Before
+    fun initRepository() {
+        stopKoin()
+        // use Koin Library as a service locator
+        val myModule = module {
+            //Declare a ViewModel - be later inject into Fragment with dedicated injector using by viewModel()
+            viewModel {
+                RemindersListViewModel(
+                    get(),
+                    get()
+                )
+            }
+            single {
+                FakeDataSource() as ReminderDataSource
+            }
+        }
+        startKoin {
+            androidContext(getApplicationContext())
+            modules(listOf(myModule))
+        }
+    }
+
+    @After
+    fun cleanupDb() = runBlockingTest { dataSource.deleteAllReminders() }
+    // Here, we're testing the ReminderList to see if it accurately displays the reminders.
+
+    // Here, we're testing the ReminderList to see what it would display in the absence of any items(reminders)
+    @Test
+    fun rMp1oViPb3EdvcJ5kxoqe52RuaiK6YiUYo() = runBlockingTest{
+        // GIVEN - Empty Database
+        dataSource.deleteAllReminders()
+        // WHEN - start Fragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+        //THEN - We don't see any items(reminders), we only see No data string
+        onView(withText(R.string.no_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withText(reminder1.title)).check(doesNotExist())
+    }
+
+    @Test
+    fun reminderListAndDisplayedInUi() = runBlockingTest {
+        //while ADD ACTIVE (INCOMPLETE) TASK TO DB FOR GIVEN
+        dataSource.saveReminder(reminder1)
+        dataSource.saveReminder(reminder2)
+        dataSource.saveReminder(reminder3)
+        // WHEN - Start Fragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+        //THEN - We see 3 items(reminders) in the list, which we added above
+        onView(withText(reminder1.title)).check(matches(isDisplayed()))
+        onView(withText(reminder2.description)).check(matches(isDisplayed()))
+        onView(withText(reminder3.title)).check(matches(isDisplayed()))
+        onView(withId(R.id.noDataTextView)).check(matches(not(isDisplayed())))
+    }
+
+
+    // Here, we're trying clickFab, and the destination we want to reach is the Reminder Fragment.
+    @Test
+    fun clickFabAndNavigateToReminderFragment() = runBlockingTest {
+        // GIVEN - On the home screen ReminderListFragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+        // WHEN - Click on the fab(float Action button)
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        // THEN - Verify that we navigate to the (save reminder fragment)
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
+    }}

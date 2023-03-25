a31f51666261b5808d18783334c7930ec3beadba
Youssef Bassem <youssef.bassem.refaat@gmail.com>
2023-03-25 07:41:58 +0200

    Update RemindersDaoTest.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -25,7 +25,62 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+private lateinit var database: RemindersDatabase
+
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun initDb() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun saveReminder_addReminder_checkitsdata() = runBlockingTest {
+        // GIVEN - insert a reminder
+        val reminder = ReminderDTO("Tit1", "Desc1", "loc1", 1.1, 1.1)
+        database.reminderDao().saveReminder(reminder)
+
+        // WHEN - find it by id
+        val load = database.reminderDao().getReminderById(reminder.id)
+
+        // THEN - check the data
+        assertThat<ReminderDTO>(load as ReminderDTO, notNullValue())
+        assertThat(load.id, `is`(reminder.id))
+        assertThat(load.title, `is`(reminder.title))
+        assertThat(load.description, `is`(reminder.description))
+        assertThat(load.location, `is`(reminder.location))
+        assertThat(load.latitude, `is`(reminder.latitude))
+        assertThat(load.longitude, `is`(reminder.longitude))
+    }
+
+    @Test
+    fun insertReminders_addRemindersAndDeleteThem_checkListIsEmpty() = runBlockingTest {
+        //Given - add some reminders to data base
+        val reminder = ReminderDTO("Tit1", "Desc1", "loc1", 1.1, 1.1)
+        val reminder2 = ReminderDTO("Tit2", "Desc2", "loc2", 2.2, 2.2)
+        val reminder3 = ReminderDTO("Tit3", "Desc3", "loc3", 3.3, 3.3)
+
+        database.reminderDao().saveReminder(reminder)
+        database.reminderDao().saveReminder(reminder2)
+        database.reminderDao().saveReminder(reminder3)
+
+        //Then - Delete add reminders from database
+        database.reminderDao().deleteAllReminders()
+
+        //Then - check the list is empty
+        val remindersList = database.reminderDao().getReminders()
+
+        assertThat(remindersList, `is`(emptyList()))
+    }
 
 
 }
